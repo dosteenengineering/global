833f80a19a74e901b53dd9097a045853be043b95
Hisham <hisham@globalsurf.ae>
2026-05-07 20:16:49 +0530

    fix: add suspense for faq page

--- app/components/client/Faq/Index.tsx
+++ app/components/client/Faq/Index.tsx
@@ -1,12 +1,15 @@
 import InnerPageBanner2 from "@/app/components/common/InnerPageBanner2";
 import { banner } from "./data";
 import FaqSection from "./Sections/Main";
+import { Suspense } from "react";
 
 const Index = () => {
   return (
     <>
       <InnerPageBanner2 {...banner} />
-      <FaqSection />
+      <Suspense fallback={<div className="h-screen bg-white" />}>
+        <FaqSection />  
+      </Suspense>
     </>
   );
 };
