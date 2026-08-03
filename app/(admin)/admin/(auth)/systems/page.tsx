da9ee7eab99f6d706cf0b2736f9aa655c58ed1de
Hisham <hisham@globalsurf.ae>
2026-08-03 17:52:16 +0530

    fix: added serach in systems in admin

--- app/(admin)/admin/(auth)/systems/page.tsx
+++ app/(admin)/admin/(auth)/systems/page.tsx
@@ -53,10 +53,11 @@ export default function Systems() {
 
   const [sector, setSector] = useState<string>("");
   const [location, setLocation] = useState<string>("");
-  const [systemList, setSystemList] = useState<{ _id: string, firstSection: { title: string, description: string } }[]>([]);
+  const [systemList, setSystemList] = useState<{ _id: string, firstSection: { title: string, description: string }, slug: string }[]>([]);
   const [locationList, setLocationList] = useState<{ _id: string, name: string }[]>([]);
   const [sectorList, setSectorList] = useState<{ _id: string, name: string }[]>([]);
   const [reorderMode, setReorderMode] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const router = useRouter();
 
@@ -119,6 +120,15 @@ export default function Systems() {
   // }
 
 
+  const filteredSystems = systemList.filter((item) => {
+  const search = searchTerm.toLowerCase();
+
+  return (
+    item.firstSection.title.toLowerCase().includes(search) ||
+    item.slug.toLowerCase().includes(search)
+  );
+});
+
   useEffect(() => {
     handleFetchSystems();
     // fetchSystemDetails();
@@ -131,7 +141,15 @@ export default function Systems() {
 
         <div className="h-screen w-full p-5 shadow-md border-black/20 rounded-md overflow-y-hidden bg-white">
           <div className="flex justify-between border-b-2 border-black/20 pb-2">
-            <Label className="text-sm font-bold">Systems</Label>
+            <div className="flex gap-5">
+              <Label className="text-sm font-bold">Systems</Label>
+              <Input
+  placeholder="Search..."
+  className="w-[300px]"
+  value={searchTerm}
+  onChange={(e) => setSearchTerm(e.target.value)}
+/>
+            </div>
             <div className="flex gap-2">
               <Button onClick={() => router.push("/admin/systems/add")} className="text-white cursor-pointer">Add System</Button>
             </div>
@@ -139,10 +157,11 @@ export default function Systems() {
           <div className="mt-2 flex flex-col gap-2 overflow-y-scroll h-[90%]">
 
 
-            {!reorderMode && systemList.map((item) => (
+            {!reorderMode && filteredSystems.map((item) => (
               <div className="flex justify-between border border-black/20 p-2 items-center rounded-md shadow-md hover:shadow-lg transition-all duration-300" key={item._id}>
-                <div className="text-[16px]">
+                <div className="text-[16px] flex flex-col gap-1">
                   {item.firstSection.title}
+                  <span className="text-xs text-gray-500">{item.slug}</span>
                 </div>
                 <div className="flex gap-5">
                   <MdEdit onClick={() => router.push(`/admin/systems/edit/${item._id}`)} />
