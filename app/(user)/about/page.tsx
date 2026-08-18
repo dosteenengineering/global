62ecf693b6c4239e1c749ade8573cbd5301582f6
rahees <raheez1995india@gmail.com>
2026-08-18 17:47:50 +0530

    about link changed and a page added under about folder

---
+++ app/(user)/about/page.tsx
@@ -0,0 +1,40 @@
+import Index from "@/app/components/client/About/Index";
+import { buildMetadata } from "@/lib/seo/buildMetadata";
+import { getGroupedCountryMapData } from "@/lib/services/getCountryMap";
+import { Metadata } from "next";
+import { headers } from "next/headers";
+
+export async function generateMetadata() {
+  const headersList = await headers();
+  const pathname =
+    headersList.get("x-pathname") ?? headersList.get("x-invoke-path") ?? "/";
+  const response = await fetch(`${process.env.BASE_URL}/api/admin/about`, {
+    next: { revalidate: 60 },
+  });
+  const { data } = await response.json();
+  if (data.seo) {
+    return buildMetadata(data.seo, pathname);
+  }
+}
+
+const page = async () => {
+  const response = await fetch(`${process.env.BASE_URL}/api/admin/about`, {
+    next: { revalidate: 60 },
+  });
+  const data = await response.json();
+
+  const countries = await getGroupedCountryMapData();
+  return (
+    <>
+      {data?.data?.seo?.schema && (
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: data.data.seo.schema }}
+        />
+      )}
+      <Index data={data.data} countries={countries} />
+    </>
+  );
+};
+
+export default page;
